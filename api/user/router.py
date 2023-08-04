3a7cb16ab00a0d13d01c3780025d8f84d16ac619
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-04 14:51:04 +0500

    deleted not used endpoints

--- api/user/router.py
+++ api/user/router.py
@@ -1,11 +1,5 @@
-
-
-
-from typing import Any, Dict
 from fastapi import APIRouter
-from api.firebaseApp import db
-from api.user.helpers import getShortData, getShortDataFromDB, isLocalAndDBShortEq, isShortDataEmpty, isShortsDataNotEq
-from api.user.userSchema import UserShortData
+from api.user.helpers import getShortData
 
 
 router = APIRouter(
@@ -13,31 +7,7 @@ router = APIRouter(
     tags=['Пользователь'],
 )
 
-@router.patch('/short')
-async def updateShortData(userId: str, data: UserShortData):
-    userRef = db.collection('users').document(userId)
-    short = {
-        'short': data.dict()
-    }
-    userSnap = await userRef.update(short)
-    return userSnap.transform_results
-    
-@router.get('/shortEmpty')
-async def updateShortData(userId: str):
-    isEmpty = await isShortDataEmpty(userId)
-    return isEmpty
-
-@router.get('/record')
-async def getUserRecord(userId: str):
-    record = await getShortDataFromDB(userId=userId)
-    return record
-
 @router.get('/short')
 async def updateShortData(userId: str):
     shortData = await getShortData(userId)
     return shortData
-
-@router.get('/shortFromDBEqToLocal')
-async def checkShortData(userId: str):
-    isEq = await isLocalAndDBShortEq(userId)
-    return isEq
