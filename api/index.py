759086873c41bdbd6517fb6863bd07584dc62a0b
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-05 11:18:20 +0500

    move init firebase app to data folder

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from firebase_admin import firestore_async, auth, credentials, initialize_app
 from datetime import datetime
 from fastapi import FastAPI
 from data.schemas import DraftShotData, ShotData
@@ -8,10 +7,6 @@ from data.helpers import checkShortData, getShortData, getUserDrafts, getUserSho
 app = FastAPI()
 
 
-cred = credentials.Certificate(cert='api/service.json')
-firebase_app = initialize_app.initialize_app(cred)
-db = firestore_async.client(app=firebase_app)
-auth = auth.Client(app=firebase_app)
 
 
 @app.get('/api/check')
