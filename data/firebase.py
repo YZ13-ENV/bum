96bfd18d823b239fabd3a503ac39f8fbcf225c16
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-06 07:34:02 +0500

    remove api folder and all .py files

--- data/firebase.py
+++
@@ -1,6 +0,0 @@
-from firebase_admin import firestore_async, auth, credentials, initialize_app
-
-cred = credentials.Certificate(cert='data/service.json')
-firebase_app = initialize_app(cred)
-db = firestore_async.client(app=firebase_app)
-auth = auth.Client(app=firebase_app)
