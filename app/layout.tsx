53be8c404df5ba54fac253ac6adc8656940890f9
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-10 20:16:11 +0500

    remove caching and changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import LayoutWrapper from '@/components/LayoutWrapper'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Nunito_Sans } from 'next/font/google'
 import { Metadata } from 'next'
 import AppHeader from '@/components/widgets/AppHeader'
 import StateProvider from '@/components/StateProvider'
 export const dynamic = 'force-dynamic'
-const inter = Inter({ subsets: ['latin'] })
+const NunitoSans = Nunito_Sans({ subsets: ['latin', 'cyrillic'], variable: '--root-font' })
 
 export const metadata: Metadata = {
   title: 'Dey',
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <StateProvider>
       <LayoutWrapper>
-        <html lang="en" className={inter.className}>
+        <html lang="en" className={`${NunitoSans.className} ${NunitoSans.variable}`}>
           <body id='root' className='flex flex-col overflow-x-hidden body_wrapper'>
             <AppHeader />
             <div className="flex flex-col w-full shrink-0 content_wrapper">
--- components/widgets/AppHeader/ui/LogoSection.tsx
+++ components/widgets/AppHeader/ui/LogoSection.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const LogoSection = () => {
     return (
         <Link href='/' className="flex items-center gap-2 shrink-0 w-fit h-fit">
-            <Image src='/Dey.svg' className='rounded-full shrink-0' width={36} height={36} alt="app-logo" />
+            <Image src='/Dey.svg' className='rounded-full shrink-0' width={32} height={32} alt="app-logo" />
             <span className='hidden text-2xl font-bold md:inline text-neutral-200'>Dey</span>
         </Link>
     )
