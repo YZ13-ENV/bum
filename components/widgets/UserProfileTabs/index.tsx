1b7d7ab787e68a756dbed6b344ee522bf8333119
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-14 19:48:59 +0500

    update styles and search section

--- components/widgets/UserProfileTabs/index.tsx
+++ components/widgets/UserProfileTabs/index.tsx
@@ -1,15 +1,18 @@
 'use client'
 import { auth } from '@/utils/app'
 import { Segmented, Button, SegmentedProps } from 'antd'
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import FollowButton from './ui/FollowButton'
+import { useRouter } from 'next/navigation'
 
 type Props = {
     profileUID: string
     shotsLength: number
 }
 const UserProfileTabs = ({ shotsLength, profileUID }: Props) => {
+    const [segment, setSegment] = useState<number>(1)
+    const router = useRouter()
     const [user] = useAuthState(auth)
     const options: SegmentedProps['options'] = [
         {
@@ -20,16 +23,26 @@ const UserProfileTabs = ({ shotsLength, profileUID }: Props) => {
             value: 2,
             label: 'Черновики',
         },
+        {
+            value: 3,
+            label: 'Редактировать'
+        }
     ].filter(opt => {
         if (user) {
-            if (user.uid !== profileUID && opt.value === 2) {
+            if (user.uid !== profileUID && opt.value !== 1) {
                 return false
             } else return true
-        } else return opt.value !== 2
+        } else return opt.value === 1
     })
+    useLayoutEffect(() => {
+        const isSegment = options.findIndex(option => parseInt(option.toString()) === segment)
+        if (isSegment) {
+            router.push(`?tab=${segment}`)
+        }
+    },[segment])
     return (
         <div className="flex justify-between w-full h-fit">
-            <Segmented options={options} size='large' />
+            <Segmented options={options} size='large' value={segment} onChange={e => setSegment(parseInt(e.toString()))} />
             <div className="flex items-center gap-2 w-fit h-fit">
                 <FollowButton profileUID={profileUID} />
             </div>
