b7682bb1ec38a4f5e3f765f391a2c35ffe54c3f3
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-16 16:05:14 +0500

    change fetch method images and videos

--- helpers/fetchFile.ts
+++ helpers/fetchFile.ts
@@ -1,7 +1,8 @@
 import { getStorageHost } from "./getHost"
 
-export const fetchFile = async(link: string): Promise<string | null> => {
+export const fetchFile = (link: string): string => {
     const stableLink = link.charAt(0) === '/' ? link.substring(1) : link
     const fetchUrl = `${getStorageHost()}/files/file?link=${stableLink}`
+    // console.log(fetchUrl)
     return fetchUrl
 }
