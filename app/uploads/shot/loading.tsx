8fed9efc7ec142d03b91d45b77de4e9b8f3305fb
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-08-22 10:29:04 +0500

    added loader for uploader and remove footer

---
+++ app/uploads/shot/loading.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <section className='flex flex-col w-full h-full shrink-0'>
+            <div className="flex items-center justify-between w-full h-12 p-2 border-b shrink-0 border-neutral-700">
+                <div className="w-24 h-full rounded-xl bg-neutral-800 animate-pulse" />
+                <div className="w-24 h-full rounded-xl bg-neutral-800 animate-pulse" />
+            </div>
+            <div className="flex flex-col justify-center w-full h-full">
+                <div className="flex flex-col w-full h-full max-w-4xl max-h-full gap-4 px-4 py-4 mx-auto overflow-y-auto md:px-0 md:py-4">
+                    <div className="w-full h-8 rounded-xl bg-neutral-800 animate-pulse"></div>
+                    <div className="w-full rounded-xl bg-neutral-800 animate-pulse aspect-[4/3]" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Loading
