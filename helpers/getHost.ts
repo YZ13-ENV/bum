7f65ad484248bc158ddf69b7caf639877dfe7e47
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-12 17:46:39 +0500

    fix host

--- helpers/getHost.ts
+++ helpers/getHost.ts
@@ -1,6 +1,6 @@
 export const getHost = () => {
-    return 'http://localhost:8000'
-    // return 'https://api.darkmaterial.space'
+    if (process.env.NODE_ENV === 'development') return 'http://localhost:8000'
+    return 'https://api.darkmaterial.space'
 }
 
 export const getStorageHost = () => {
