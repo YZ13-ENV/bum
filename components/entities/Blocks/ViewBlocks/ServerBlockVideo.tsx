d89119c32432d7e72b8dbfa9db3f893b35361af8
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-12 17:43:22 +0500

    added in beta ambiLight for subscriber

--- components/entities/Blocks/ViewBlocks/ServerBlockVideo.tsx
+++ components/entities/Blocks/ViewBlocks/ServerBlockVideo.tsx
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
 type Props = {
+    withAmbiLight?: boolean
     block: VideoBlock
     autoPlay?: boolean
 }
@@ -12,13 +13,13 @@ type Props = {
 //     const url = await fetchFile(link)
 //     return url
 // }
-const ServerBlockVideo = async({ block, autoPlay }: Props) => {
+const ServerBlockVideo = async({ block, withAmbiLight=false, autoPlay=false }: Props) => {
     return (
         <div className="relative w-full h-full rounded-xl">
             <Suspense fallback={<div className='w-full h-full'/>}>
                 {
                     block.link
-                    ? <LoadedVideo link={fetchFile(block.link)} autoPlay={autoPlay} />
+                    ? <LoadedVideo link={fetchFile(block.link)} withAmbiLight={withAmbiLight} autoPlay={autoPlay} />
                     : <div className='w-full h-full rounded-xl bg-neutral-900' />
                 }
             </Suspense>
---
+++ components/widgets/AmbiLight/VideoAmbiLight.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { LoadedVideoProps } from '@/components/shared/LoadedVideo'
+import { useInterval } from 'ahooks';
+import { ElementRef, useLayoutEffect, useRef, useState } from 'react'
+
+const VideoAmbiLight = ({ link, autoPlay }: Omit<LoadedVideoProps, 'withAmbiLight'>) => {
+    const videoBlock = useRef<ElementRef<'video'>>(null);
+    const canvas = useRef<ElementRef<'canvas'>>(null);
+    const [run, setRun] = useState<boolean>(false);
+    const FRAMERATE = 30;
+    function repaintAmbilight() {
+        if (canvas.current) {
+            const context = canvas.current.getContext("2d");
+            if (context && videoBlock.current) {
+                context.drawImage(videoBlock.current, 0, 0, videoBlock.current.videoWidth, videoBlock.current.videoHeight);
+            }
+        }
+    }
+    
+    function startAmbilightRepaint() {
+        if (videoBlock.current && canvas.current) {
+            setRun(true)
+        }
+    }
+    const stopAmbilightRepaint = useInterval(() => {
+        repaintAmbilight()
+      }, run ? 1000 / FRAMERATE : undefined);
+    useLayoutEffect(() => {
+        if (videoBlock.current) {
+            videoBlock.current.play()
+            videoBlock.current.addEventListener("play", () => startAmbilightRepaint());
+            videoBlock.current.addEventListener("pause", () => stopAmbilightRepaint());
+            videoBlock.current.addEventListener("ended", () => stopAmbilightRepaint());
+            videoBlock.current.addEventListener("seeked", () => repaintAmbilight());
+            videoBlock.current.addEventListener("load", () => repaintAmbilight());
+        }
+    },[videoBlock.current])
+    return (
+        <>
+            <canvas ref={canvas} id="ambiLight" onLoad={() => repaintAmbilight()} />
+            <video ref={videoBlock} src={link} muted className='object-cover w-full h-full rounded-xl' loop autoPlay={autoPlay} controls={false} />
+        </>
+    )
+}
+
+export default VideoAmbiLight
