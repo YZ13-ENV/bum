20236f16c118869fd31df588f7888d782fe75a01
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-12 17:54:44 +0500

    fix MediaBlock Props

--- components/entities/shot/index.tsx
+++ components/entities/shot/index.tsx
@@ -12,8 +12,8 @@ const ShotCard = ({ shot }: Props) => {
         <ShotWrapper shot={shot}>
             {
                 shot.thumbnail 
-                ? <MediaBlock {...{ link: shot.thumbnail.link, type: 'image' }} server quality={100} object='cover' autoPlay={false} />
-                : shot.rootBlock.link !== '' ? <MediaBlock {...shot.rootBlock} server quality={75} object='cover' autoPlay={false} />
+                ? <MediaBlock {...{ link: shot.thumbnail.link, type: 'image' }} quality={100} object='cover' autoPlay={false} />
+                : shot.rootBlock.link !== '' ? <MediaBlock {...shot.rootBlock} quality={75} object='cover' autoPlay={false} />
                 : <div className='flex flex-col items-center justify-center w-full h-full'>
                     <span className='text-xs text-center text-neutral-400'>Нет обложки</span>
                 </div>
@@ -25,8 +25,8 @@ const ShotCard = ({ shot }: Props) => {
             <Link href={`/${shot.authorId}/${shot.doc_id}`} className='relative w-full aspect-[4/3] h-full'>
                 {
                     shot.thumbnail
-                    ? <MediaBlock {...{ link: shot.thumbnail.link, type: 'image' }} server quality={100} object='cover' autoPlay={false} />
-                    : <MediaBlock {...shot.rootBlock} server quality={75} object='cover' autoPlay={false} />
+                    ? <MediaBlock {...{ link: shot.thumbnail.link, type: 'image' }} quality={100} object='cover' autoPlay={false} />
+                    : <MediaBlock {...shot.rootBlock} quality={75} object='cover' autoPlay={false} />
                 }
             </Link>
         </ShotWrapper>
