bd1cede77e742646e41d5602e51a9186c0be3bc6
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-18 14:08:18 +0500

    fix ui

--- app/[userId]/[shotId]/loading.tsx
+++ app/[userId]/[shotId]/loading.tsx
@@ -1,9 +1,8 @@
-import UserSectionLoader from '@/components/shared/Loaders/ShotPage/UserSectionLoader'
 const Loading = () => {
     return (
         <div className='relative flex flex-col w-full min-h-full gap-6 px-4 pb-4 md:px-0 h-fit'>
             <div className="flex flex-col w-full max-w-4xl gap-4 mx-auto h-fit shrink-0">
-                <UserSectionLoader />
+                <div className="w-full h-12 max-w-md mx-auto rounded-xl bg-neutral-900"></div>
                 <div className="w-full rounded-xl aspect-[4/3] shrink-0 bg-neutral-900 animate-pulse"></div>
                 <div className="flex md:flex-row flex-col items-start w-full max-w-4xl gap-2 mx-auto h-fit min-h-[48rem]">
                     <div className="flex flex-col w-full gap-2 md:h-full h-fit md:w-8/12">
