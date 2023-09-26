b07922e03c174c5820443e79f23ebd01ed224c48
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-26 15:59:15 +0500

    fix history and statistics cards

--- app/[userId]/history/page.tsx
+++ app/[userId]/history/page.tsx
@@ -51,7 +51,7 @@ const HistoryCard = async({ historyUnit }: CardProps) => {
         <div className="flex flex-col w-full gap-2 border md:h-48 h-fit md:flex-row shrink-0 rounded-xl border-neutral-800">
             <div className="relative h-full aspect-[4/3] rounded-xl bg-neutral-900">
                 <MediaBlock link={fetchFile(stableLink as string)} object='cover'
-                type={(stableLink as string).endsWith('.mp4') ? 'image' : 'video'} />
+                type={(stableLink as string).endsWith('.mp4') ? 'video' : 'image'} />
             </div>
             <div className="flex flex-col w-full h-full gap-2 p-4">
                 { process.env.NODE_ENV === 'development' ? DateTime.fromSeconds(historyUnit.createdAt).toISO() : null }
