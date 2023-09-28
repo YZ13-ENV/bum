f7235cd1f08c9eaa2bb386236c1a0cb6d7e32e78
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-28 09:44:28 +0500

    added remoteConfig to subLabel

--- components/shared/SubLabel.tsx
+++ components/shared/SubLabel.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client'
+import { app } from '@/utils/app'
+import { fetchAndActivate, getRemoteConfig, getString } from 'firebase/remote-config'
+import React, { useEffect, useState } from 'react'
 
 const SubLabel = () => {
-  return <span className='px-2 py-1 text-xs text-black bg-white rounded-md'>Плюс</span>
+  const [tag, setTag] = useState<string>('')
+  useEffect(() => {
+      const remoteConfig = getRemoteConfig(app);
+      remoteConfig.settings.minimumFetchIntervalMillis = 3600000;
+      fetchAndActivate(remoteConfig)
+      .then(() => {
+          const bum_string = getString(remoteConfig, 'sub_tag')
+          setTag(bum_string)
+      })
+      .catch((err) => {
+      });
+  },[])
+  return <span className='px-2 py-1 text-xs text-black bg-white rounded-md'>{tag || 'Плюс'}</span>
 }
 
 export default SubLabel
