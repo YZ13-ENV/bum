ff6a2b88bc3e2d651e5657b70456348923e69b6a
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-09-30 19:45:30 +0500

    fix tags page

--- app/[userId]/recommendations/page.tsx
+++ app/[userId]/recommendations/page.tsx
@@ -1,16 +1,7 @@
-import { DocShotData, ShotData } from '@/types'
-import { db } from '@/utils/app'
-import { collectionGroup, getDocs } from 'firebase/firestore'
 import React from 'react'
 import { flatten, uniq } from 'lodash'
 import TagsGround from './TagsGround'
-
-export const getAllShot = async() => {
-    const collRef = collectionGroup(db, 'shots')
-    const snaps = await getDocs(collRef)
-    const convertedSnaps = snaps.docs.map(snap => ({ ...snap.data() as ShotData, doc_id: snap.id } as DocShotData))
-    return convertedSnaps
-}
+import { getAllShot } from '@/app/fetchers'
 
 const RecommendationsPage = async() => {
     const shots = await getAllShot()
--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,26 +1,25 @@
 import { uniq, flatten } from 'lodash'
 import React from 'react'
-import { getAllShot } from '../[userId]/recommendations/page'
 import { BiRightArrowAlt } from 'react-icons/bi'
 import Link from 'next/link'
+import { getAllShot } from '../fetchers'
 
 const TagsPage = async() => {
     const shots = await getAllShot()
-    const onlyTags = uniq(flatten(shots.map(shot => shot.tags)))
+    const onlyTags = shots.length > 0 ? uniq(flatten(shots.map(shot => shot.tags))) : []
     return (
         <div className='flex flex-col w-full max-w-5xl gap-4 px-4 mx-auto'>
             <div className="flex items-center justify-center w-full h-fit">
                 <h1 className='text-2xl font-semibold text-center text-neutral-200'>Тэги</h1>
             </div>
             {
+                onlyTags.length > 0 &&
                 onlyTags
                 .sort()
                 .map(tag => 
                     <Link href={`/tags/${tag}`} key={tag} 
                     className='flex items-start justify-between w-full h-24 p-4 transition-colors border cursor-pointer rounded-xl border-neutral-700 hover:bg-neutral-900'>
-                        <h1 className='text-xl font-bold text-neutral-200'>
-                            {tag[0]}<span className='text-base font-medium text-neutral-300'>{tag.substring(1, tag.length)}</span>
-                        </h1>
+                        <h1 className='text-base font-medium capitalize text-neutral-300'>{tag}</h1>
                         <BiRightArrowAlt size={19} />
                     </Link>
                 )
