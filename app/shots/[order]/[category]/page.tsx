87cb52e809de54d97c9252a6157919c2e9266b35
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-10-02 17:53:02 +0500

    fix shots page chunker

--- app/shots/[order]/[category]/page.tsx
+++ app/shots/[order]/[category]/page.tsx
@@ -12,7 +12,7 @@ const ShotsByCategoryPage = ({ params }: Props) => {
     return (
         <Suspense fallback={<div className="flex items-center justify-center w-full h-full shot_wrapper"><BiLoaderAlt size={17} className="animate-spin" /></div>}>
             {/* {`/shots/${params.order}`} */}
-            <Chunker order={`${params.order}/${params.category}`} countPrefix='/shots/v2/chunkByCategoriesCount/' shotsPrefix='/shots/v2/chunkByCategories/' />
+            <Chunker order={`${params.category}/${params.order}`} countPrefix='/shots/v2/chunkByCategoriesCount/' shotsPrefix='/shots/v2/chunkByCategories/' />
         </Suspense>
     )
 }
