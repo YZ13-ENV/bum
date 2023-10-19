dc1740cb9dca9d53561e2fbac21aaa80b5944e24
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-10-19 09:10:25 +0500

    add responsive for uploader

--- app/uploads/shot/page.tsx
+++ app/uploads/shot/page.tsx
@@ -5,22 +5,30 @@ import FinalTouchModal from "@/components/widgets/FinalTouchModal"
 // import UploadBlocksMenu from "@/components/widgets/UploadModule/BlockSideMenu"
 // import PrevWorks from "@/components/widgets/UploadModule/PrevWork"
 import UploadBlockView from "@/components/widgets/UploadModule"
+import Link from "next/link"
 
 const UploadShotPage = () => {
+    const sidesWrapper = "sticky lg:flex hidden flex-col h-full gap-2 p-4 top-4 xl:w-96 lg:w-80 md:w-72 w-64 shrink-0"
     return (
-        <section className='flex flex-col w-full min-h-screen shrink-0 bg-neutral-950'>
-            <FinalTouchModal />
-            <UploadHeader />
-            <div className="flex w-full h-full">
-                <div className="sticky flex flex-col h-full gap-2 p-4 top-4 w-96 shrink-0">
-                    <LeftSide />
-                </div>
-                <UploadBlockView />
-                <div className="sticky flex flex-col h-full gap-2 p-4 top-4 w-96 shrink-0">
-                    <RightSide />
-                </div>
+        <>
+            <div className="flex flex-col items-center justify-center w-full min-h-screen gap-4 md:hidden">
+                <span className="text-sm text-center text-neutral-300">Конструктор недоступен на мобильных устройствах</span>
+                <Link href='/' className="px-4 py-2 text-sm text-black bg-white rounded-lg">Вернуться</Link>
             </div>
-        </section>
+            <section className='relative flex-col hidden w-full min-h-screen md:flex shrink-0 bg-neutral-950'>
+                <FinalTouchModal />
+                <UploadHeader />
+                <div className="flex w-full h-full">
+                    <div className={sidesWrapper}>
+                        <LeftSide />
+                    </div>
+                    <UploadBlockView />
+                    <div className={sidesWrapper}>
+                        <RightSide />
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
--- components/entities/uploader/ui/UploadHeader.tsx
+++ components/entities/uploader/ui/UploadHeader.tsx
@@ -14,8 +14,8 @@ const UploadHeader = () => {
     const dispatch = useAppDispatch()
     return (
         <div className="flex items-center justify-between w-full gap-2 p-2 h-14">
-            <div className="flex flex-row items-center gap-4 px-2 w-fit h-fit">
-                <Link href='/'><Image src={'/bum.svg'} width={32} height={32} alt='bum-logo' /></Link>
+            <div className="flex flex-row items-center gap-4 px-2 shrink-0 w-fit h-fit">
+                <Link href='/'><Image src={'/bum.svg'} width={32} className='shrink-0' height={32} alt='bum-logo' /></Link>
                 <div className="flex items-center gap-2 px-3 py-2 mx-auto border rounded-lg w-fit h-fit bg-neutral-900 border-neutral-700">
                     <BiFileBlank size={19} className='mb-0.5' />
                     <span className='text-sm text-neutral-300'>Черновики</span>
@@ -23,10 +23,7 @@ const UploadHeader = () => {
                     <span className='text-sm text-neutral-300'>{draftId || 'Пустой черновик'}</span>
                 </div>
             </div>
-            <div className="flex items-center justify-center w-full h-full max-w-6xl gap-2 shrink-0">
-                {/* <BlocksGrid /> */}
-            </div>
-            <div className="flex flex-row-reverse items-center gap-2 px-2 md:flex-row w-fit h-fit">
+            <div className="flex flex-row items-center justify-end gap-2 px-2 w-fit shrink-0 h-fit">
                 <Button disabled={draft.rootBlock.link === '' || draft.title === ''} size='large'
                 onClick={() => dispatch(setFinalTouchModal(true))} type='primary'>{isTabletOrMobile ? <BiRightArrowAlt size={17} /> :'Продолжить'}</Button>
             </div>
