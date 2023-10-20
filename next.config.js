127d288eefc008b108967779cd5cd8de9e01f067
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2023-10-20 10:31:55 +0500

    changed position for session picker, added limit fot chunks

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  swcMinify: true,
-  compress: true,
   images: {
     unoptimized: false,
     formats: ['image/webp'], 
     domains: [
       'cdn.darkmaterial.space',
     ]
-  },
-  experimental: {
-    workerThreads: true,
-    cpus: 4,
   }
 }
